dea35958ec9d67a31997a1746bae03dbed48605a
Larissa Conti <laricbarros@gmail.com>
2025-10-31 12:57:01 -0300

    adicionado comentário com respostas às perguntas - final da quinta refatoração

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,24 @@ package com.gildedrose;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 1. Princípio Aberto-Fechado: sim, é possível estender o sistema sem modificar o código existente.
+ * Para adicionar um novo item, apenas é necessário:
+ *  - Criar uma nova classe referente ao item desejado que implemente ItemUpdater e
+ *  - Adicionar uma entrada no mapa de updaters no metodo initializeUpdaters()
+ * Não é preciso modificar a lógica principal em updateQuality(); inclusive é desencorajado fazer isso.
+
+ * 2. Princípio da Responsabilidade Única: sim, cada itemUpdater tem uma única responsabilidade
+ * Cada implementação (AgedBrieUpdater, SulfurasUpdater, etc.) é responsável apenas
+ * pelas regras de atualização de seu respectivo tipo de item.
+ * GildedRose é responsável apenas pela coordenação dos updaters.
+
+ * 3. Princípio de Substituição de Liskov: respeitado até o momento.
+ * As implementações de ItemUpdater podem ser substituídas sem alterar o comportamento esperado do sistema,
+ * desde que sigam o contrato definido pela interface ItemUpdater.
+ * Comportamento padrão evita que itens não especificados quebrem o sistema.
+ */
+
 class GildedRose {
     Item[] items;
     private final Map<String, ItemUpdater> updaters;
