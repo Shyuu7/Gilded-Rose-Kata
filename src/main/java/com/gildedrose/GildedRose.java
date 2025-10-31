4fcbed61e2c409c4f5094e298bc02c2b472629cf
Larissa Conti <laricbarros@gmail.com>
2025-10-30 22:26:56 -0300

    segunda etapa da refatoração completa

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,79 +1,28 @@
 package com.gildedrose;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class GildedRose {
     Item[] items;
+    private final Map<String, ItemUpdater> updaters;
 
     public GildedRose(Item[] items) {
         this.items = items;
+        this.updaters = new HashMap<>();
+        initializeUpdaters();
+    }
+
+    private void initializeUpdaters() {
+        updaters.put("Sulfuras, Hand of Ragnaros", new SulfurasUpdater());
+        updaters.put("Aged Brie", new AgedBrieUpdater());
+        updaters.put("Backstage passes to a TAFKAL80ETC concert", new BackstagePassUpdater());
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            boolean isSulfuras = item.name.equals("Sulfuras, Hand of Ragnaros");
-            boolean isAgedBrie = item.name.equals("Aged Brie");
-            boolean isBackstagePasses = item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-
-            if (isSulfuras) {
-                updateSulfuras(item);
-            } else if (isAgedBrie) {
-                updateAgedBrie(item);
-            } else if (isBackstagePasses) {
-                updateBackstagePass(item);
-            } else {
-                updateNormalItem(item);
-            }
-        }
-    }
-
-
-    private void updateSulfuras(Item item) {
-        //Sulfuras não perde qualidade nem precisa ser vendido
-    }
-
-    private void updateAgedBrie(Item item) {
-        decreaseSellIn(item);
-        increaseQuality(item);
-        if (item.sellIn < 0) {
-            increaseQuality(item);
-        }
-    }
-
-    private void updateBackstagePass(Item item) {
-        decreaseSellIn(item);
-        if (item.sellIn < 0) {
-            item.quality = 0;
-        } else {
-            increaseQuality(item);
-            if (item.sellIn < 10) {
-                increaseQuality(item);
-            }
-            if (item.sellIn < 5) {
-                increaseQuality(item);
-            }
-        }
-    }
-
-    private void updateNormalItem(Item item) {
-        decreaseSellIn(item);
-        decreaseQuality(item);
-        if (item.sellIn < 0) {
-            decreaseQuality(item);
-        }
-    }
-
-    private void decreaseSellIn(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
+            ItemUpdater updater = updaters.getOrDefault(item.name, new DefaultUpdater());
+            updater.updateItem(item);
         }
     }
 }
